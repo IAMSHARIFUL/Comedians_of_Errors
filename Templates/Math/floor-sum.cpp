20e503819daa4c06930db409864c802a61a65bc8
IAMSHARIFUL <1705119@ugrad.cse.buet.ac.bd>
2023-01-24 13:20:27 +0600

    Duplicate floor sum removed

--- Templates/Math/floor-sum.cpp
+++
@@ -1,8 +0,0 @@
-ll FloorSumAP(ll a, ll b, ll c, ll n){
-  if(!a) return (b / c) * (n + 1);
-  if(a >= c or b >= c) 
-      return ( ( n * (n + 1) ) / 2) * (a / c) + (n + 1) * (b / c) 
-      + FloorSumAP(a % c, b % c, c, n);
-  ll m = (a * n + b) / c;
-  return m * n - FloorSumAP(c, c - b - 1, a, m - 1);
-}
