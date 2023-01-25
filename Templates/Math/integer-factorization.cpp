88566cd3137b8cfca1c6402fb21892f54daee9c3
neo11235 <63189687+neo11235@users.noreply.github.com>
2023-01-25 23:50:49 +0600

    Update integer-factorization.cpp

--- Templates/Math/integer-factorization.cpp
+++ Templates/Math/integer-factorization.cpp
@@ -1,57 +1,87 @@
-long long pollards_p_minus_1(long long n) {
-    int B = 10; long long g = 1;
-    while (B <= 1000000 && g < n) {
-        long long a = 2 + rand() %  (n - 3); g = gcd(a, n);
-        if (g > 1) return g;
-        // compute a^M
-        for (int p : primes) {
-            if (p >= B) continue; long long p_power = 1;
-            while (p_power * p <= B) p_power *= p;
-            a = power(a, p_power, n); g = gcd(a - 1, n);
-            if (g > 1 && g < n) return g;
-        }
-        B *= 2;
-    }
-    return 1;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+namespace Rho {
+ULL mult(ULL a, ULL b, ULL mod) {
+  LL ret = a * b - mod * (ULL)(1.0L/mod*a*b);
+  return ret+mod*(ret<0) - mod*(ret>=(LL) mod);
 }
-long long mult(long long a, long long b, long long mod) {
-    long long result = 0;
-    while (b) {
-        if (b & 1) result = (result + a) % mod;
-        a = (a + a) % mod; b >>= 1;
-    }
-    return result;
+ULL power(ULL x, ULL p, ULL mod){
+  ULL s=1, m=x;
+  while(p) {
+    if(p&1) s = mult(s, m, mod);
+    p>>=1; m = mult(m, m, mod);
+  } return s;
 }
-long long f(long long x, long long c, long long mod) {
-    return (mult(x, x, mod) + c) % mod;
+vector<LL> bases =
+{2,325, 9375, 28178, 450775, 9780504, 1795265022};
+bool isprime(LL n) {
+  if (n<2)  return 0;
+  if (n%2==0)   return n==2;
+  ULL s = __builtin_ctzll(n-1), d = n>>s;
+  for (ULL x: bases) {
+    ULL p = power(x%n, d, n), t = s;
+    while (p!=1&&p!=n-1&&x%n&&t--) p=mult(p,p,n);
+    if (p!=n-1 && t != s)         return 0;
+  } return 1;
 }
-long long rho(long long n, long long x0=2, long long c=1) {
-    long long x = x0; long long y = x0; long long g = 1;
-    while (g == 1) {
-        x = f(x, c, n); y = f(y, c, n); y = f(y, c, n);
-        g = gcd(abs(x - y), n);
-    }
-    return g;
+mt19937_64 rng(chrono::system_clock::
+                now().time_since_epoch().count());
+ULL FindFactor(ULL n) {
+  if (n == 1 || isprime(n))  return n;
+  ULL c=1, x=0, y=0, t=0, prod = 2, x0 = 1, q;
+  auto f = [&](ULL X) { return mult(X, X, n) + c;};
+  while (t++ % 128 or __gcd(prod, n) == 1) {
+    if (x == y) c = rng()%(n-1)+1, x = x0, y=f(x);
+    if ((q=mult(prod, max(x, y) - min(x, y), n)))
+      prod = q;
+    x = f(x), y = f(f(y));
+  } return __gcd(prod, n);
 }
-long long brent(long long n, long long x0=2, long long c=1) {
-    long long x = x0; long long g = 1; long long q = 1;
-    long long xs, y; int m = 128; int l = 1;
-    while (g == 1) {
-        y = x; int k = 0;
-        for (int i = 1; i < l; i++) x = f(x, c, n); 
-        while (k < l && g == 1) {
-            xs = x;
-            for (int i = 0; i < m && i < l - k; i++) {
-                x = f(x, c, n); q = mult(q, abs(y - x), n);
-            }
-            g = gcd(q, n); k += m;
-        }
-        l *= 2;
-    }
-    if (g == n) {
-        do {
-            xs = f(xs, c, n); g = gcd(abs(xs - y), n);
-        } while (g == 1);
-    }
-    return g;
+vector<ULL> factorize(ULL x) {
+  if (x == 1)   return {};
+  ULL a = FindFactor(x), b = x/a;
+  if (a == x) return {a};
+  vector<ULL> L = factorize(a), R = factorize(b);
+  L.insert(L.end(), R.begin(), R.end());
+  return L;
+} }typedef long long LL;
+typedef unsigned long long ULL;
+
+namespace Rho {
+ULL mult(ULL a, ULL b, ULL mod) {
+  LL ret = a * b - mod * (ULL)(1.0L/mod*a*b);
+  return ret+mod*(ret<0) - mod*(ret>=(LL) mod);
 }
+ULL power(ULL x, ULL p, ULL mod){
+  ULL s=1, m=x;
+  while(p) {
+    if(p&1) s = mult(s, m, mod);
+    p>>=1; m = mult(m, m, mod);
+  } return s;
+}
+vector<LL> bases =
+{2,325, 9375, 28178, 450775, 9780504, 1795265022};
+bool isprime(LL n) {
+  if (n<2)  return 0;
+  if (n%2==0)   return n==2;
+  ULL s = __builtin_ctzll(n-1), d = n>>s;
+  for (ULL x: bases) {
+    ULL p = power(x%n, d, n), t = s;
+    while (p!=1&&p!=n-1&&x%n&&t--) p=mult(p,p,n);
+    if (p!=n-1 && t != s)         return 0;
+  } return 1;
+}
+mt19937_64 rng(chrono::system_clock::
+                now().time_since_epoch().count());
+ULL FindFactor(ULL n) {
+  if (n == 1 || isprime(n))  return n;
+  ULL c=1, x=0, y=0, t=0, prod = 2, x0 = 1, q;
+  auto f = [&](ULL X) { return mult(X, X, n) + c;};
+  while (t++ % 128 or __gcd(prod, n) == 1) {
+    if (x == y) c = rng()%(n-1)+1, x = x0, y=f(x);
+    if ((q=mult(prod, max(x, y) - min(x, y), n)))
+      prod = q;
+    x = f(x), y = f(f(y));
+  } return __gcd(prod, n);
+}}
