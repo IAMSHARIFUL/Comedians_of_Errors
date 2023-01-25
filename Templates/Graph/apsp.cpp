bdbdde8d436a4251d1d4966d00e2b35e61c529b2
neo11235 <63189687+neo11235@users.noreply.github.com>
2023-01-26 00:10:31 +0600

    Create apsp.cpp

---
+++ Templates/Graph/apsp.cpp
@@ -0,0 +1,8 @@
+ll g[500][500];
+void floyedWarshal(int n){
+    //g[i][i] should be zero
+    for(int k=0;k<n;k++)
+    for(int i=0;i<n;i++)
+    for(int j=0;j<n;j++)
+    if(g[i][k]<LLONG_MAX&&g[k][j]<LLONG_MAX)
+    g[i][j]=min(g[i][j],g[i][k]+g[k][j]);}
