804153cf173c1399cecff3f907c97b81078f0130
IAMSHARIFUL <1705119@ugrad.cse.buet.ac.bd>
2023-03-02 22:27:46 +0600

    Discrete Log: Previous Function was wrong in CP-Algo :3

--- Templates/Math/discrete-root.cpp
+++ Templates/Math/discrete-root.cpp
@@ -68,7 +68,7 @@ int DiscreteLog(int a, int b, int m) {
     int n = sqrt(m) + 1;
     map<int, int> vals;
     for (int p = 1; p <= n; ++p)
-        vals[powmod(a,(int) (1ll* p * n) %m , m)] = p;
+        vals[powmod(a, p * n, m)] = p;
     for (int q = 0; q <= n; ++q) {
         int cur = (powmod(a, q, m) * 1ll * b) % m;
         if (vals.count(cur)) {
