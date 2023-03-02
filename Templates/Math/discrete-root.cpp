49b22712e08a5b492645f84d6e6dff96146899d0
IAMSHARIFUL <1705119@ugrad.cse.buet.ac.bd>
2023-03-03 02:30:43 +0600

    Even current Discrete Log in CP Algo has some bugs. So it's taken from Hasan

--- Templates/Math/discrete-root.cpp
+++ Templates/Math/discrete-root.cpp
@@ -63,20 +63,28 @@ int PrimitiveRoot(int p){
     }
     return -1;
 }
-int DiscreteLog(int a, int b, int m) {
-    a %= m, b %= m;
-    int n = sqrt(m) + 1;
-    map<int, int> vals;
-    for (int p = 1; p <= n; ++p)
-        vals[powmod(a, p * n, m)] = p;
-    for (int q = 0; q <= n; ++q) {
-        int cur = (powmod(a, q, m) * 1ll * b) % m;
-        if (vals.count(cur)) {
-            int ans = vals[cur] * n - q;
-            return ans;
+ll DiscreteLog(ll a,ll b,ll mod){
+    ll n,p,q;
+    n=(ll)sqrt((double)mod)+1ll;
+    ll an=powmod(a,n,mod);
+    map<ll,ll> map_;
+    ll cur=an;
+    for(p=1;p<=n;p++){
+        if(map_.find(cur)==map_.end()){
+            map_.insert({cur,p});
         }
+        cur=(cur*an)%mod;
     }
-    return -1;
+    cur=b;
+    ll mn=LLONG_MAX;
+    for(q=0;q<=n;q++){
+        if(map_.find(cur)!=map_.end()){
+            mn=min((n*map_[cur])-q,mn);
+        }
+        cur=(cur*a)%mod;
+    }
+    if(mn==LLONG_MAX)return -1;
+    else return mn;
 }
 vector<int> DiscreteRoot(int n,int a,int k){
     int g = PrimitiveRoot(n);
