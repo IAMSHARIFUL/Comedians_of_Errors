e48ddecc498b2ba05a79d6358651042ace452660
IAMSHARIFUL <1705119@ugrad.cse.buet.ac.bd>
2023-03-09 23:58:59 +0600

    Euler Path changed

--- Templates/Graph/min-cost-max-flow.cpp
+++ Templates/Graph/min-cost-max-flow.cpp
@@ -1,32 +1,26 @@
 namespace MCMF {
     typedef long long F; typedef long long C;
     const F infF = 1e18; const C infC = 1e18;
-
     const int N = 5005;
     typedef pair<C, F> PCF;
-
     struct Edge {int frm, to; C cost; F cap, flow;};
-
     int n, s, t;
     vector<Edge> edges;
     vector<int> adj[N];
     C pi[N], dis[N];
     F fl[N];
     int prv[N], vis[N];
-
     void init(int nodes, int source, int sink) {
         n = nodes, s = source, t = sink;
         for (int i=0; i<n; i++) pi[i] = 0, adj[i].clear();
         edges.clear();
     }
-
     void addEdge(int u, int v, F cap,C cost) {
         edges.push_back({u, v, cost, cap, 0});
         edges.push_back({v, u, -cost, 0, 0});
         adj[u].push_back(edges.size()-2);
         adj[v].push_back(edges.size()-1);
     }
-
     bool SPFA() {
         for (int i=0; i<n; i++) {
             dis[i] = infC; fl[i] = 0;
@@ -35,25 +29,22 @@ namespace MCMF {
         queue<int> q;
         q.push(s);
         dis[s] = 0; fl[s] = infF; vis[s] = 1;
-
         while (!q.empty()) {
             int u = q.front(); q.pop();
             vis[u] = 0;
             for (int eid : adj[u]) {
                 Edge &e = edges[eid];
                 if (e.cap == e.flow) continue;
-
                 if (dis[u] + e.cost < dis[e.to]) {
                     dis[e.to] = dis[u] + e.cost;
                     fl[e.to] = min(fl[u], e.cap - e.flow);
                     prv[e.to] = eid^1;
-                    if (!vis[e.to])     q.push(e.to);
+                    if (!vis[e.to])  q.push(e.to);
                 }
             }
         }
         return fl[t] > 0;
     }
-
     PCF solveSPFA() {
         C cost = 0; F flow = 0;
         while (SPFA()) {
@@ -66,12 +57,10 @@ namespace MCMF {
         }
         return {cost, flow};
     }
-
     void normalize() {
         SPFA();
         for (int i=0; i<n; i++) pi[i] = dis[i];
     }
-
     bool Dijkstra() {
         for (int i=0; i<n; i++) {
             dis[i] = infC; fl[i] = 0;
@@ -80,12 +69,10 @@ namespace MCMF {
         priority_queue<pair<C, int>> pq;
         pq.emplace(0, s);
         dis[s] = 0; fl[s] = infF;
-
         while (!pq.empty()) {
             int u = pq.top().second; pq.pop();
             if (vis[u]) continue;
             vis[u] = 1;
-
             for (int eid : adj[u]) {
                 Edge &e = edges[eid];
                 if (vis[e.to] || e.cap == e.flow) continue;
@@ -101,7 +88,6 @@ namespace MCMF {
         }
         return fl[t] > 0;
     }
-
     PCF solveDijkstra() {
         normalize();
         C cost = 0; F flow = 0;
@@ -110,7 +96,6 @@ namespace MCMF {
                 if (fl[i])     pi[i] += dis[i];
             C pathcost = pi[t]-pi[s];
             cost += pathcost*fl[t]; flow += fl[t];
-
             for (int u=t, e=prv[u]; e!=-1; u=edges[e].to, e=prv[u]) {
                 edges[e].flow -= fl[t];
                 edges[e^1].flow += fl[t];
