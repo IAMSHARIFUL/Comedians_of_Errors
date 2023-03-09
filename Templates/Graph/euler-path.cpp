c5ab5ab5111b081319ee19eb5c0418fb73bbafa9
neo11235 <63189687+neo11235@users.noreply.github.com>
2023-03-09 23:51:51 +0600

    removed space

--- Templates/Graph/euler-path.cpp
+++ Templates/Graph/euler-path.cpp
@@ -1,47 +1,35 @@
-class EulerGraph
-{
+class EulerGraph{
 public:
-    struct Edge
-    {
+    struct Edge{
         int other;
         int oi;
         bool vis;
-        Edge(int other, int oi): other(other), oi(oi){ vis=false; }
-    };
-
+        Edge(int other, int oi): other(other), oi(oi){ vis=false; }};
     vector<vector<Edge>> adj;
     int* cnt;
     bool* sig;
-    EulerGraph(int n)
-    {
+    EulerGraph(int n){
         adj.resize(n);
         cnt=new int[n];
         sig=new bool[n];
         memset(cnt, 0, sizeof(int)*n);
-        memset(sig, 0, n);
-    }
-
+        memset(sig, 0, n);}
     void addEdge(int u, int v){
         int un=adj[u].size(), vn=adj[v].size();
-        adj[u].eb(v, vn), adj[v].eb(u, un);
-    }
-
+        adj[u].eb(v, vn), adj[v].eb(u, un);}
     deque<int> getCircuit(int src){
         deque<int> circuit;
         circuit.push_back(src);
-
         int ecnt=adj[src].size();
         while(ecnt){
             int v=circuit.back();
             sig[v]=true;
             if(cnt[v]==adj[v].size()){
                 circuit.pop_back();
-                circuit.push_front(circuit.back());
-            }
+                circuit.push_front(circuit.back());}
             else if(adj[v][cnt[v]].vis){
                 cnt[v]++;
-                ecnt--;
-            }
+                ecnt--;}
             else {
                 int other=adj[v][cnt[v]].other;
                 int oi=adj[v][cnt[v]].oi;
@@ -51,10 +39,5 @@ public:
                 if(!sig[other]) ecnt+=adj[other].size();
 
                 cnt[v]++;
-                ecnt--;
-            }
-        }
-        return circuit;
-    }
-
-};
+                ecnt--;}}
+        return circuit;}};
