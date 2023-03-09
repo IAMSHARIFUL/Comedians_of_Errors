213f20dbe282d16136717014857cca36a1207049
ahshafi <76455395+ahshafi@users.noreply.github.com>
2023-03-09 23:23:12 +0600

    Update min-cost-max-flow.cpp

--- Templates/Graph/min-cost-max-flow.cpp
+++ Templates/Graph/min-cost-max-flow.cpp
@@ -1,41 +1,121 @@
-struct Edge{ int from, to, capacity, cost; };
-vector<vector<int>> adj, cost, capacity;
-const int INF = 1e9;
-void shortest_paths(int n,int v0,vector<int>&d,vector<int>& p){
-    d.assign(n, INF); d[v0] = 0; vector<bool> inq(n, false);
-    queue<int> q; q.push(v0); p.assign(n, -1);
-    while (!q.empty()) {
-        int u = q.front(); q.pop(); inq[u] = false;
-        for (int v : adj[u]) {
-            if (capacity[u][v] > 0 && d[v] > d[u]+cost[u][v]){
-                d[v] = d[u] + cost[u][v]; p[v] = u;
-                if (!inq[v]) { inq[v] = true; q.push(v); }
+namespace MCMF {
+    typedef long long F; typedef long long C;
+    const F infF = 1e18; const C infC = 1e18;
+
+    const int N = 5005;
+    typedef pair<C, F> PCF;
+
+    struct Edge {int frm, to; C cost; F cap, flow;};
+
+    int n, s, t;
+    vector<Edge> edges;
+    vector<int> adj[N];
+    C pi[N], dis[N];
+    F fl[N];
+    int prv[N], vis[N];
+
+    void init(int nodes, int source, int sink) {
+        n = nodes, s = source, t = sink;
+        for (int i=0; i<n; i++) pi[i] = 0, adj[i].clear();
+        edges.clear();
+    }
+
+    void addEdge(int u, int v, F cap,C cost) {
+        edges.push_back({u, v, cost, cap, 0});
+        edges.push_back({v, u, -cost, 0, 0});
+        adj[u].push_back(edges.size()-2);
+        adj[v].push_back(edges.size()-1);
+    }
+
+    bool SPFA() {
+        for (int i=0; i<n; i++) {
+            dis[i] = infC; fl[i] = 0;
+            vis[i] = 0; prv[i] = -1;
+        }
+        queue<int> q;
+        q.push(s);
+        dis[s] = 0; fl[s] = infF; vis[s] = 1;
+
+        while (!q.empty()) {
+            int u = q.front(); q.pop();
+            vis[u] = 0;
+            for (int eid : adj[u]) {
+                Edge &e = edges[eid];
+                if (e.cap == e.flow) continue;
+
+                if (dis[u] + e.cost < dis[e.to]) {
+                    dis[e.to] = dis[u] + e.cost;
+                    fl[e.to] = min(fl[u], e.cap - e.flow);
+                    prv[e.to] = eid^1;
+                    if (!vis[e.to])     q.push(e.to);
+                }
             }
         }
+        return fl[t] > 0;
     }
-}
-int min_cost_flow(int N,vector<Edge> edges,int K,int s,int t) {
-adj.assign(N,vector<int>());cost.assign(N,vector<int>(N,0));
-    capacity.assign(N, vector<int>(N, 0));
-    for (Edge e : edges) {
-    adj[e.from].push_back(e.to); adj[e.to].push_back(e.from);
-    cost[e.from][e.to] = e.cost; cost[e.to][e.from] = -e.cost;
-    capacity[e.from][e.to] = e.capacity; }
-    int flow = 0; int cost = 0;
-    vector<int> d, p;
-    while (flow < K) {
-        shortest_paths(N, s, d, p); if (d[t] == INF) break;
-        // find max flow on that path
-        int f = K - flow; int cur = t;
-        while (cur != s) {
-            f = min(f, capacity[p[cur]][cur]); cur = p[cur];
+
+    PCF solveSPFA() {
+        C cost = 0; F flow = 0;
+        while (SPFA()) {
+            C pathcost = dis[t];
+            cost += pathcost*fl[t]; flow += fl[t];
+            for (int u=t, e=prv[u]; e!=-1; u=edges[e].to, e=prv[u]) {
+                edges[e].flow -= fl[t];
+                edges[e^1].flow += fl[t];
+            }
         }
-        // apply flow
-        flow += f; cost += f * d[t]; cur = t;
-        while (cur != s) {
-        capacity[p[cur]][cur] -= f; capacity[cur][p[cur]]+= f;
-            cur = p[cur]; }
+        return {cost, flow};
     }
-    if (flow < K) return -1;
-    else return cost;
+
+    void normalize() {
+        SPFA();
+        for (int i=0; i<n; i++) pi[i] = dis[i];
+    }
+
+    bool Dijkstra() {
+        for (int i=0; i<n; i++) {
+            dis[i] = infC; fl[i] = 0;
+            vis[i] = 0; prv[i] = -1;
+        }
+        priority_queue<pair<C, int>> pq;
+        pq.emplace(0, s);
+        dis[s] = 0; fl[s] = infF;
+
+        while (!pq.empty()) {
+            int u = pq.top().second; pq.pop();
+            if (vis[u]) continue;
+            vis[u] = 1;
+
+            for (int eid : adj[u]) {
+                Edge &e = edges[eid];
+                if (vis[e.to] || e.cap == e.flow) continue;
+
+                C nw = dis[u] + e.cost - pi[e.to] + pi[u];
+                if (nw < dis[e.to]) {
+                    dis[e.to] = nw;
+                    fl[e.to] = min(fl[u], e.cap - e.flow);
+                    prv[e.to] = eid^1;
+                    pq.emplace(-dis[e.to], e.to);
+                }
+            }
+        }
+        return fl[t] > 0;
+    }
+
+    PCF solveDijkstra() {
+        normalize();
+        C cost = 0; F flow = 0;
+        while (Dijkstra()) {
+            for (int i=0; i<n; i++)
+                if (fl[i])     pi[i] += dis[i];
+            C pathcost = pi[t]-pi[s];
+            cost += pathcost*fl[t]; flow += fl[t];
+
+            for (int u=t, e=prv[u]; e!=-1; u=edges[e].to, e=prv[u]) {
+                edges[e].flow -= fl[t];
+                edges[e^1].flow += fl[t];
+            }
+        }
+        return {cost, flow};
+    }
 }
