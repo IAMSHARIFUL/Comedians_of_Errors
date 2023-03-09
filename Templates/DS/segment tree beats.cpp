5b9f3da6264dcc57b5d86da3a3c4e64fc22c8dc2
IAMSHARIFUL <1705119@ugrad.cse.buet.ac.bd>
2023-03-09 13:13:37 +0600

    Coloured Template. But digits are not colours specially :(

--- Templates/DS/segment tree beats.cpp
+++ Templates/DS/segment tree beats.cpp
@@ -1,27 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-
 const int MAXN = 200001;  // 1-based
-
 int N;
 ll A[MAXN];
-
 struct Node {
-	ll sum;  // Sum tag
-	ll max1;  // Max value
-	ll max2;  // Second Max value
-	ll maxc;  // Max value count
-	ll min1;  // Min value
-	ll min2;  // Second Min value
-	ll minc;  // Min value count
-	ll lazy;  // Lazy tag
+	ll sum,max1,max2,maxc,min1,
+    min2,minc,lazy;
+    //maxi->ith max,maxc->maxcount
 } T[MAXN * 4];
-
 void merge(int t) {
 	// sum
 	T[t].sum = T[t << 1].sum + T[t << 1 | 1].sum;
-
 	// max
 	if (T[t << 1].max1 == T[t << 1 | 1].max1) {
 		T[t].max1 = T[t << 1].max1;
@@ -38,7 +28,6 @@ void merge(int t) {
 			T[t].maxc = T[t << 1 | 1].maxc;
 		}
 	}
-
 	// min
 	if (T[t << 1].min1 == T[t << 1 | 1].min1) {
 		T[t].min1 = T[t << 1].min1;
@@ -56,11 +45,9 @@ void merge(int t) {
 		}
 	}
 }
-
 void push_add(int t, int tl, int tr, ll v) {
-	if (v == 0) {
+	if (v == 0)
 		return;
-	}
 	T[t].sum += (tr - tl + 1) *v;
 	T[t].max1 += v;
 	if (T[t].max2 != -llINF) {
@@ -72,12 +59,10 @@ void push_add(int t, int tl, int tr, ll v) {
 	}
 	T[t].lazy += v;
 }
-
 // corresponds to a chmin update
 void push_max(int t, ll v, bool l) {
-	if (v >= T[t].max1) {
+	if (v >= T[t].max1)
 		return;
-	}
 	T[t].sum -= T[t].max1 * T[t].maxc;
 	T[t].max1 = v;
 	T[t].sum += T[t].max1 * T[t].maxc;
@@ -91,12 +76,10 @@ void push_max(int t, ll v, bool l) {
 		}
 	}
 }
-
 // corresponds to a chmax update
 void push_min(int t, ll v, bool l) {
-	if (v <= T[t].min1) {
+	if (v <= T[t].min1)
 		return;
-	}
 	T[t].sum -= T[t].min1 * T[t].minc;
 	T[t].min1 = v;
 	T[t].sum += T[t].min1 * T[t].minc;
@@ -110,7 +93,6 @@ void push_min(int t, ll v, bool l) {
 		}
 	}
 }
-
 void pushdown(int t, int tl, int tr) {
 	if (tl == tr)
 		return;
@@ -119,16 +101,13 @@ void pushdown(int t, int tl, int tr) {
 	push_add(t << 1, tl, tm, T[t].lazy);
 	push_add(t << 1 | 1, tm + 1, tr, T[t].lazy);
 	T[t].lazy = 0;
-
 	// max
 	push_max(t << 1, T[t].max1, tl == tm);
 	push_max(t << 1 | 1, T[t].max1, tm + 1 == tr);
-
 	// min
 	push_min(t << 1, T[t].min1, tl == tm);
 	push_min(t << 1 | 1, T[t].min1, tm + 1 == tr);
 }
-
 void build(int t=1, int tl=0, int tr=N-1) {
 	T[t].lazy = 0;
 	if (tl == tr) {
@@ -138,29 +117,24 @@ void build(int t=1, int tl=0, int tr=N-1) {
 		T[t].min2 = llINF;
 		return;
 	}
-
 	int tm = (tl + tr) >> 1;
 	build(t << 1, tl, tm);
 	build(t << 1 | 1, tm + 1, tr);
 	merge(t);
 }
-
 void update_add(int l, int r, ll v, int t=1, int tl=0, int tr=N-1) {
-	if (r < tl || tr < l) {
+	if (r < tl || tr < l)
 		return;
-	}
 	if (l <= tl && tr <= r) {
 		push_add(t, tl, tr, v);
 		return;
 	}
 	pushdown(t, tl, tr);
-
 	int tm = (tl + tr) >> 1;
 	update_add(l, r, v, t << 1, tl, tm);
 	update_add(l, r, v, t << 1 | 1, tm + 1, tr);
 	merge(t);
 }
-
 void update_chmin(int l, int r, ll v, int t=1, int tl=0, int tr=N-1) {
 	if (r < tl || tr < l || v >= T[t].max1) {
 		return;
@@ -170,45 +144,35 @@ void update_chmin(int l, int r, ll v, int t=1, int tl=0, int tr=N-1) {
 		return;
 	}
 	pushdown(t, tl, tr);
-
 	int tm = (tl + tr) >> 1;
 	update_chmin(l, r, v, t << 1, tl, tm);
 	update_chmin(l, r, v, t << 1 | 1, tm + 1, tr);
 	merge(t);
 }
-
 void update_chmax(int l, int r, ll v, int t=1, int tl=0, int tr=N-1) {
-	if (r < tl || tr < l || v <= T[t].min1) {
+	if (r < tl || tr < l || v <= T[t].min1)
 		return;
-	}
 	if (l <= tl && tr <= r && v < T[t].min2) {
 		push_min(t, v, tl == tr);
 		return;
 	}
 	pushdown(t, tl, tr);
-
 	int tm = (tl + tr) >> 1;
 	update_chmax(l, r, v, t << 1, tl, tm);
 	update_chmax(l, r, v, t << 1 | 1, tm + 1, tr);
 	merge(t);
 }
-
 ll query_sum(int l, int r, int t=1, int tl=0, int tr=N-1) {
-	if (r < tl || tr < l) {
+	if (r < tl || tr < l)
 		return 0;
-	}
-	if (l <= tl && tr <= r) {
+	if (l <= tl && tr <= r)
 		return T[t].sum;
-	}
 	pushdown(t, tl, tr);
-
 	int tm = (tl + tr) >> 1;
 	return query_sum(l, r, t << 1, tl, tm) + query_sum(l, r, t << 1 | 1, tm + 1, tr);
 }
-
 int main() {
 	int Q;
-
 	cin >> N >> Q;
 	for (int i = 0; i < N; i++) {
 		cin >> A[i];
